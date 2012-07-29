12db9bb0d0cfa97ca9b1cd74bb485c9d5a31af0a
unknown-user <unknown-user@fe80::bd24:bbdb:1cd4:dcef%25>
2012-07-29 20:35:03 +0800

    test in monodevelop

--- HelloCsharp/HelloCsharp/Main.cs
+++ HelloCsharp/HelloCsharp/Main.cs
@@ -2,41 +2,42 @@ using System;
 
 namespace HelloCsharp
 {
-	class MainClass
-	{
-		public static void Main (string[] args)
-		{
-			Console.WriteLine ("Hello CSharp!");
-			Operation op = new Operation();
-			Console.WriteLine("20 + 10 = " + op.Addition(20, 10) );
-			Console.WriteLine("20 x 10 = " + op.Multiply(20, 10) );
-			Console.WriteLine("20 - 10 = " + op.Substraction(20,10));
-			Console.WriteLine("20 / 10 = " + op.Division(20, 10));
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello CSharp!");
+            Operation op = new Operation();
+            Console.WriteLine("20 + 10 = " + op.Addition(20, 10));
+            Console.WriteLine("20 x 10 = " + op.Multiply(20, 10));
+            Console.WriteLine("20 - 10 = " + op.Substraction(20, 10));
+            Console.WriteLine("20 / 10 = " + op.Division(20, 10));
 
-			Student stud = new Student();
-			stud.PrintStudentInfo();
+            Student stud = new Student();
+            stud.PrintStudentInfo();
 
-			Console.WriteLine("Merge test to master!");
-		}
-	}
+            Console.WriteLine("Merge test to master!");
+            Console.WriteLine("try to develop in monodevelop!");
+        }
+    }
 
-	class Operation
-	{
-		public int Addition(int a, int b)
-		{
-			return a + b;
-		}
-		public int Substraction(int a, int b)
-		{
-			return a - b;
-		}
-		public int Multiply(int a, int b)
-		{
-			return a * b;
-		}
-		public int Division(int a, int b)
-		{
-			return a/b;
-		}
-	}
+    class Operation
+    {
+        public int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        public int Substraction(int a, int b)
+        {
+            return a - b;
+        }
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public int Division(int a, int b)
+        {
+            return a / b;
+        }
+    }
 }
