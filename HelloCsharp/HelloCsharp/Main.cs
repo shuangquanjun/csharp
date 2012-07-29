226a971763b519e1a10c12a47ddd0314ac3c48fd
shuangquanjun <shaugnquanjun@gmail.com>
2012-07-29 14:02:10 +0800

    add operation class

--- HelloCsharp/HelloCsharp/Main.cs
+++ HelloCsharp/HelloCsharp/Main.cs
@@ -7,6 +7,31 @@ namespace HelloCsharp
 		public static void Main (string[] args)
 		{
 			Console.WriteLine ("Hello CSharp!");
+			Operation op = new Operation();
+			Console.WriteLine("20 + 10 = " + op.Addition(20, 10) );
+			Console.WriteLine("20 x 10 = " + op.Multiply(20, 10) );
+			Console.WriteLine("20 - 10 = " + op.Substraction(20,10));
+			Console.WriteLine("20 / 10 = " + op.Division(20, 10));
+		}
+	}
+
+	class Operation
+	{
+		public int Addition(int a, int b)
+		{
+			return a + b;
+		}
+		public int Substraction(int a, int b)
+		{
+			return a - b;
+		}
+		public int Multiply(int a, int b)
+		{
+			return a * b;
+		}
+		public int Division(int a, int b)
+		{
+			return a/b;
 		}
 	}
 }
